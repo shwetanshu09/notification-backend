3580b7d2a0ee00a5878f21ea16a3414a4830a1eb
shwetanshu09 <vikrant13.c@icloud.com>
2026-02-08 17:39:51 -0600

    Update WebConfig.java

--- src/main/java/com/wba/wallboard/notification/config/WebConfig.java
+++ src/main/java/com/wba/wallboard/notification/config/WebConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowed-origins}")
+    @Value("${cors.allowed-origins:http://localhost:4200}")
     private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(allowedOrigins.split(","))
+                .allowedOriginPatterns(allowedOrigins.split(","))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
